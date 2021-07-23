10e89372635430662f463e22f174a5ec370d12ba
lestere312 <lestere@oregonstate.edu>
2021-07-23 13:43:24 -0700

    More queries

--- assets/queries.sql
+++ assets/queries.sql
@@ -2,7 +2,26 @@
 -- Accounts Page--
 --
 
---Main table info
+-- Sum of unique cards
+SELECT SUM(quantity) FROM `Cards`
+JOIN DeckCards ON Cards.cardID = DeckCards.cardID
+WHERE deckID IN(SELECT deckID  FROM `Decks`
+WHERE userID = 1)
+
+--Number of Decks
+SELECT COUNT(deckID) FROM `Decks`
+WHERE userID = 1
+
+--Number of Collections
+SELECT COUNT(collectionID) FROM `Collections`
+WHERE userID = 1
+
+--Total collection price
+SELECT SUM(quantity*cardPrice) FROM `Cards`
+JOIN CollectionCards ON Cards.cardID = CollectionCards.cardID
+WHERE collectionID IN(SELECT collectionID  FROM `Decks`
+WHERE userID = 1)
+
 
 --****************
 -- Decks Page--
@@ -11,6 +30,11 @@
 --Name of each deck of user
 SELECT deckName FROM `Decks`
 WHERE userID = 1
+
+--Total Cards in a deck
+SELECT SUM(quantity)
+FROM `DeckCards`
+WHERE deckID = 1
 
 --Main table info
 SELECT * FROM `Cards`
@@ -24,6 +48,17 @@ AND deckID = 1
 --Name of each collection of user
 SELECT collectionName FROM `Collections`
 WHERE userID = 1
+
+--Total Cards in a collection
+SELECT SUM(quantity)
+FROM `CollectionCards`
+WHERE collectionID = 1
+
+--Total collection value
+SELECT SUM(quantity*cardPrice) FROM `Cards`
+JOIN CollectionCards ON Cards.cardID = CollectionCards.cardID
+WHERE collectionID IN(SELECT collectionID  FROM `Decks`
+WHERE collectionID = 1)
 
 --Main table info
 SELECT * FROM `Cards`
