23d1568e4fdcf6b88ed8d286e974070a3fbf4ef7
lestere312 <lestere@oregonstate.edu>
2021-07-23 11:11:49 -0700

    more queries

--- assets/queries.sql
+++ assets/queries.sql
@@ -1,18 +1,33 @@
+--
 --This hopefully tells you every card in all of one accounts decks
 --this should hopefully work for collections too.
+--
 
 SELECT * FROM `Cards`
 WHERE cardID IN(SELECT cardID FROM `DeckCards`
 WHERE deckID IN(SELECT deckID  FROM `Decks`
 WHERE userID = 1))
+--better version
+SELECT * FROM `Cards`
+JOIN DeckCards ON Cards.cardID = DeckCards.cardID
+WHERE deckID IN(SELECT deckID  FROM `Decks`
+WHERE userID = 1)
 
+--
 --Gets cards from specific deck
+--
 
 SELECT * FROM `Cards`
 WHERE cardID IN(SELECT cardID FROM `DeckCards`
 WHERE deckID = 1)
+--Better version
+SELECT * FROM `Cards`
+JOIN DeckCards ON Cards.cardID = DeckCards.cardID
+AND deckID = 1
 
+--
 --List of deck names by a user
+--
 
 SELECT deckName FROM `Decks`
 WHERE userID = 1
