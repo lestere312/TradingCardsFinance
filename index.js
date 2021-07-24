9ea2c1820685ba5ca3a601b915a938395a8444cd
lestere312 <lestere@oregonstate.edu>
2021-07-24 14:06:42 -0700

    starting handlebars

--- index.js
+++ index.js
@@ -1,13 +1,54 @@
-const express = require('express');
-const path = require('path');
-const fs = require('fs')
+var express = require('express');
+var app = express();
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
-const http = require('http');
 
-const app = express();
+
+var exphbs = require('express-handlebars');
+const { query } = require('express');
+app.engine('.hbs', exphbs({
+    extname: ".hbs"
+}));
+app.set('view engine', '.hbs');
 
 app.use(express.static('public'));
 
 const PORT = process.env.PORT || 3007;
+
+app.get('/', function(req, res)
+{
+            return res.render('index');
+});
+
+app.get('/cards', function(req, res)
+{
+            return res.render('cards');
+});
+
+app.get('/deck', function(req, res)
+{
+            return res.render('deck');
+});
+
+app.get('/collection', function(req, res)
+{
+            return res.render('collection');
+});
+
+app.get('/login', function(req, res)
+{
+            return res.render('login');
+});
+
+app.get('/account', function(req, res)
+{
+            return res.render('account');
+});
+
+app.get('/accountCreation', function(req, res)
+{
+            return res.render('accountCreation');
+});
 
 app.listen(PORT, () => console.log('server started on port:' + PORT));
